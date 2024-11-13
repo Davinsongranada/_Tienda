96241e4bdf51ef299ff75300603fac8da5ae6720
Davinson Granada <118221608+Davinsongranada@users.noreply.github.com>
2024-11-13 16:36:01 -0500

    Organizando el home

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.example.myapplication;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
-
+    ImageView bEnseres, bInicio, bAparatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,24 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bEnseres = findViewById(R.id.btnEnseres);
+        bInicio = findViewById(R.id.btnInicio);
+        bAparatos = findViewById(R.id.btnAparatos);
+
+        bEnseres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bEnseres.setColorFilter(ContextCompat.getColor(HomeActivity.this, R.color.orange), PorterDuff.Mode.SRC_IN);
+            }
+        });
+
+        bInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
