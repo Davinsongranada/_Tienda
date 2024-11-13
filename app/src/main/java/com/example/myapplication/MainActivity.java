1273aa6a06b2fd5296408f554a00c94e90448327
Davinson Granada <118221608+Davinsongranada@users.noreply.github.com>
2024-11-13 13:56:53 -0500

    Terminé el código de inicio de sesión

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnReg;
+    Button btnReg, btnLog;
+    EditText rEmail, rPassword;
+    TextView tvMessageR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +38,53 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
+
+        rEmail = findViewById(R.id.idEmail);
+        rPassword = findViewById(R.id.idPassword);
+        btnLog = findViewById(R.id.btnLogIn);
         btnReg = findViewById(R.id.btnRegister);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),SingInActivity.class));
+            }
+        });
+
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = rEmail.getText().toString();
+                String password = rPassword.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()){
+                mAuth
+                        .signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                tvMessageR.setTextColor(Color.parseColor("#800000"));
+                                tvMessageR.setText("No se creo la cuenta");
+                            }
+                        });
+
+                } else{
+                    tvMessageR.setTextColor(Color.parseColor("#800000"));
+                    tvMessageR.setText("Debe ingresar todos los datos");
+                }
+
             }
         });
     }
